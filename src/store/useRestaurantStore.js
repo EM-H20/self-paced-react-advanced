28778c8d34bed1b3af2dc7852ebba6d38f8bd6ca
EM-H20 <78837018+EM-H20@users.noreply.github.com>
2026-07-03 18:13:20 +0900

    fix: 머지로 되돌아간 App과 스토어를 복구 #72

--- src/store/useRestaurantStore.js
+++ src/store/useRestaurantStore.js
@@ -1,21 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
-import { BASE_URL } from "../constants/api.js";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const fetchRestaurants = async () => {
-  const response = await fetch(BASE_URL);
-  const data = await response.json();
-  if (!response.ok) throw data;
-  return data;
-};
+const useRestaurantStore = create(
+  persist(
+    (set) => ({
+      category: "전체",
 
-export default function useGetRestaurants() {
-  return useQuery({
-    queryKey: ["restaurants"],
-    queryFn: fetchRestaurants,
-    staleTime: 1 * 60 * 1000,
-    gcTime: 5 * 60 * 1000,
+      actions: {
+        setCategory: (category) => {
+          set({ category });
+        },
+      },
+    }),
+    {
+      name: "restaurant-filter",
+      partialize: (state) => ({ category: state.category }),
+    },
+  ),
+);
 
-    //여러 사용자가 하나의 서버를 공유하고, 식당 목록이 주기적으로 업데이트되는 환경. 멀티 유저 가정 때문에 추가
-    refetchInterval: 1 * 60 * 1000,
-  });
-}
+export const useCategory = () => useRestaurantStore((state) => state.category);
+export const useSetCategory = () =>
+  useRestaurantStore((state) => state.actions.setCategory);
